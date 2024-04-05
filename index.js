6e63d94eb1a71f38bba57c6686f728d71def3abe
Zachary Perlmutter <zrp200@gmail.com>
2024-04-04 23:09:47 -0700

    fix it so that args are not all lumped together, and that help works properly
    now global args are just handled at the first legal combination.

--- index.js
+++ index.js
@@ -3,57 +3,64 @@ const yargs = require("yargs")
 
 const {download, turnSpec, awaitSync} = require("./util.jsx")
 
-let {argv: {_: argv, bulk = true, headless = true, ...global_opts}} = replayArgs(process.argv.slice(2))
-    .parserConfiguration({"unknown-options-as-args": true})
-    .option('bulk', {
+const global = {
+    bulk: {
         alias: 'b',
         describe: "How many instances to run at once, if giving more than one argument",
-    })
-    .boolean('headless')
-
-delete global_opts.$0 // no one cares
+    },
+    headless: { boolean: true }
+};
 
 // options configurable per replay. also configurable globally
-function replayArgs(argv) {
-    return yargs(argv)
-        .option("reverse", {
-            alias: 'r',
-            describe: 'reverse',
-            type: 'boolean',
-            //default: false,
-        })
-        .option('show', {choices: [false, 'teams', "chat"],})
-        .option("gif", {
-            describe: "generate a gif with this input",
-            type: 'boolean',
-            //default: true,
-        })
-        .option("speed",
-            {
-                describe: 'gamespeed',
-                choices: ['very slow', 'slow', 'normal', 'fast', 'hyperfast']
-            })
-        .option('vspeed',
-            {
-                describe: 'output video speed',
-                //default: 1,
-                type: 'number',
-            })
-        .option('turns', {
-            desc: 'Show the turn indicator, default true',
-            type: "boolean"
-            // default true
-        })
+const save = {
+    'reverse': {
+        alias: 'r',
+        describe: 'reverse',
+        type: 'boolean',
+        group: 'save',
+        //default: false,
+    },
+    'show': {choices: [false, 'teams', "chat"], group: 'save'},
+    'gif': {
+        describe: "generate a gif with this input",
+        type: 'boolean',
+        group: 'save',
+        //default: true,
+    },
+    'speed': {
+        describe: 'gamespeed',
+        choices: ['very slow', 'slow', 'normal', 'fast', 'hyperfast'],
+        group: 'save'
+    },
+    'vspeed': {
+        describe: 'output video speed',
+        //default: 1,
+        type: 'number',
+        group: 'save',
+    },
+    'turns': {
+        desc: 'Show the turn indicator, default true',
+        type: "boolean",
+        group: 'save',
+        // default true
+    }
 }
 
-const browser = launch({headless});
+yargs()
+    .options(global)
+    .options(save)
+    .strictOptions() // verify all options are legitimate
+    .parse(process.argv)
 
+let {argv: {_: argv, bulk = true, headless = true}} = yargs(process.argv.slice(2))
+    .parserConfiguration({"unknown-options-as-args": true})
+    .options(global)
+
+const browser = launch({headless});
 // split parts into sections
 const parts = function* () {
-    let parser = replayArgs()
-        .help(false)
-        .version(false)
-    let last = {...global_opts}, i = 0, j = argv.length;
+    let parser = yargs().options(save)
+    let last = {}, i = 0, j = argv.length;
     while (i < j) {
         let {_: [src, turns, ...other], $0, ...opts} = parser.parse(argv.slice(i, j--));
         if (other.length || !src) continue;
@@ -74,6 +81,7 @@ const parts = function* () {
         j = argv.length;
     }
 }();
+
 // parallelism check
 let actions = [...parts].map((value,) => async () => download({browser, ...value}))
 let n = parts.length === 1 || (bulk || bulk === 1) && (bulk >= parts.length || bulk === true ? true : Math.ceil(parts.length / bulk));
