Zrp200/pscapture

commit 9833a4df118284ab94c5f8f9cdcea99713711e96

Changed how start step is calculated, implemented direct seek
* start step is now specifically the first major step or divider before -- this ensures that messages are played properly at the start
* the first turn is loaded by messing with the stepQueue

util.jsx
@@ -78,24 +78,27 @@ async function download(
         id = name ? `${battleID}_${name}` : battleID;
     }
 
+    const turnMatcher = RegExp('(?=turn\\\\|)\\d+')
     const steps = await battle.evaluate((b) => b.stepQueue);
     let startStep = start && steps.indexOf(`|turn|${start}`)
     if (step1) {
-        const turnMatcher = RegExp('(?=turn\\\\|)\\d+')
-        while (startStep < steps.length) {
-            const step = steps[startStep].substring(1)
+        let i = startStep
+        while (i < steps.length) {
+            const step = steps[i].substring(1)
             if (end && step === (`turn|${end+1}`)) {
                 // not found
-                startStep = steps.length;
+                i = steps.length;
                 break;
             }
             // update start for faster startup if we weren't given start
             const m = turnMatcher.exec(step)
             if (m) start = parseInt(m[0]);
             if (step.startsWith(step1)) break;
-            startStep++;
+            // record the last major step or divider before this one
+            if (!step || !step.startsWith('-')) startStep = i;
+            i++;
         }
-        if (startStep === steps.length) throw Error(`${id}: start not found!`)
+        if (i === steps.length) throw Error(`${id}: start not found!`)
     }
     // end of queue (exclusive)
     const endStep = (end || step2) &&  (() => {
@@ -116,25 +119,28 @@ async function download(
         return 0;
     })()
     //console.log({id, startStep, endStep})
-    if (!step1) startStep = 0
     // cut the queue at the end if needed
     if (endStep) {
         steps.splice(endStep)
-        await battle.evaluate((b, q) => b.setQueue(q), steps)
+        await battle.evaluate((b, q) => b.stepQueue = q, steps)
     }
 
     // -- start logic; find start step
     // todo maybe if I mess with the queue, I can get it to seek the start aspect directly.
-    if (start) {
+    if (start && !step1) {
+        // direct seek if we are able
         await battle.evaluate((b, start) => b.seekTurn(start, true), start)
+    } else if (startStep) {
+        // seek the exact start step by messing with the queue
+        await battle.evaluate((b, q)=> {
+            const orig = b.stepQueue
+            // cut the queue at the point we want to seek to
+            b.stepQueue = q
+            b.seekTurn(Infinity, true)
+            b.stepQueue = orig
+            b.atQueueEnd = false
+        }, steps.toSpliced(startStep+1))
     }
-
-    // jump to current step
-    while(true) if(await battle.evaluate(b => {
-        b.play() // call next step
-        b.pause() // halt for processing
-        return b.currentStep;
-    }) >= startStep) break;
 
     let state = new EventEmitter()
 

commit 60d89102977e66f2209ea263a1dc02f36f740b11

move seeking after settings are set and selectors are loaded
* This fixes a regression in 9833a4df118284ab94c5f8f9cdcea99713711e96 causing graphical errors and not hiding opponent's nicknames.

* removed outdated todo

util.jsx
@@ -152,27 +152,7 @@ async function download(
         await battle.evaluate((b, q) => b.stepQueue = q, steps)
     }
 
-    // -- start logic; find start step
-    // todo maybe if I mess with the queue, I can get it to seek the start aspect directly.
-    if (start) {
-        if (start.turn && !start.step) {
-            // direct seek if we are able
-            await battle.evaluate((b, start) => b.seekTurn(start, true), start.turn)
-        } else if (startStep) {
-            // seek the exact start step by messing with the queue
-            await battle.evaluate((b, q)=> {
-                const orig = b.stepQueue
-                // cut the queue at the point we want to seek to
-                b.stepQueue = q
-                b.seekTurn(Infinity, true)
-                b.stepQueue = orig
-                b.atQueueEnd = false
-            }, steps.toSpliced(startStep+1))
-        }
-    }
-
     let state = new EventEmitter()
-
     const battleEnd = new Promise(resolve => state.on('atqueueend', resolve))
 
     // -- options/setup
@@ -220,6 +200,24 @@ async function download(
                     box.width -= l.width + r.width
                     return box
                 })
+
+    // -- start logic; find start step
+    if (start) {
+        if (start.turn && !start.step) {
+            // direct seek if we are able
+            await battle.evaluate((b, start) => b.seekTurn(start, true), start.turn)
+        } else if (startStep) {
+            // seek the exact start step by messing with the queue
+            await battle.evaluate((b, q)=> {
+                const orig = b.stepQueue
+                // cut the queue at the point we want to seek to
+                b.stepQueue = q
+                b.seekTurn(Infinity, true)
+                b.stepQueue = orig
+                b.atQueueEnd = false
+            }, steps.toSpliced(startStep+1))
+        }
+    }
 
     // -- recording logic
     await mkdir[WEBM]; // just ensure that it's done